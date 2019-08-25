a1791ea25ec2eafbdf728361281e2b1dc1eaddc9
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-08-25 18:34:52 +0300

    implement checks whether a macro is defined at a specific position in a M4 script so that source operations can output more accurate results

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,41 @@ impl Display for MacroInvocationToken {
     }
 }
 
+fn is_first_arg_of_invocation_with_name<'a>(
+    invocation: &'a MacroInvocationToken,
+    invocation_name: &str,
+    first_arg_name: &str,
+) -> bool {
+    if invocation.name == invocation_name {
+        if let Some(first_arg) = invocation.args.first() {
+            if let Some(Token::Syntax(syntax)) = first_arg.tokens.first() {
+                match syntax {
+                    SyntaxToken::MacroInvocation(inner_invocation) => {
+                        if inner_invocation.name == first_arg_name {
+                            return true;
+                        }
+                    }
+                    SyntaxToken::QuotedString(quoted_string) => {
+                        if quoted_string == first_arg_name {
+                            return true;
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+    false
+}
+
+fn is_definition<'a>(invocation: &'a MacroInvocationToken, macro_name: &str) -> bool {
+    is_first_arg_of_invocation_with_name(invocation, "define", macro_name)
+}
+
+fn is_undefinition<'a>(invocation: &'a MacroInvocationToken, macro_name: &str) -> bool {
+    is_first_arg_of_invocation_with_name(invocation, "undefine", macro_name)
+}
+
 impl Source {
     pub fn get_macro_definition<'a>(
         &'a self,
@@ -82,24 +117,8 @@ impl Source {
         for token in self.tokens.iter() {
             if let Token::Syntax(syntax) = token {
                 if let SyntaxToken::MacroInvocation(invocation) = syntax {
-                    if invocation.name == "define" {
-                        if let Some(first_arg) = invocation.args.first() {
-                            if let Some(Token::Syntax(syntax)) = first_arg.tokens.first() {
-                                match syntax {
-                                    SyntaxToken::MacroInvocation(inner_invocation) => {
-                                        if inner_invocation.name == macro_name {
-                                            return Some(invocation);
-                                        }
-                                    }
-                                    SyntaxToken::QuotedString(quoted_string) => {
-                                        if quoted_string == macro_name {
-                                            return Some(invocation);
-                                        }
-                                    }
-                                    _ => {}
-                                }
-                            }
-                        }
+                    if is_definition(invocation, macro_name) {
+                        return Some(invocation);
                     }
                 }
             }
@@ -109,11 +128,16 @@ impl Source {
 
     pub fn get_macro_invocations<'a>(&'a self, macro_name: &str) -> Vec<&'a MacroInvocationToken> {
         let mut invocations = vec![];
+        let mut is_defined = false;
         for token in self.tokens.iter() {
             if let Token::Syntax(syntax) = token {
                 if let SyntaxToken::MacroInvocation(invocation) = syntax {
-                    if invocation.name == macro_name {
+                    if is_defined && invocation.name == macro_name {
                         invocations.push(invocation);
+                    } else if is_definition(&invocation, macro_name) {
+                        is_defined = true;
+                    } else if is_undefinition(&invocation, macro_name) {
+                        is_defined = false;
                     }
                 }
             }
@@ -122,23 +146,36 @@ impl Source {
     }
 
     pub fn rename_macro<'a>(&'a mut self, macro_name: &str, new_macro_name: &str) {
+        let mut is_defined = false;
         for token in self.tokens.iter_mut() {
             if let Token::Syntax(syntax) = token {
                 if let SyntaxToken::MacroInvocation(invocation) = syntax {
-                    if invocation.name == macro_name {
+                    if is_defined && invocation.name == macro_name {
                         invocation.name = new_macro_name.to_string();
                     } else if invocation.name == "define" {
                         if let Some(first_arg) = invocation.args.first_mut() {
                             if let Some(first_token) = first_arg.tokens.first_mut() {
                                 if let Token::Syntax(syntax) = first_token {
-                                    if let SyntaxToken::MacroInvocation(invocation) = syntax {
-                                        if invocation.name == macro_name {
-                                            invocation.name = new_macro_name.to_string();
+                                    match syntax {
+                                        SyntaxToken::MacroInvocation(inner_invocation) => {
+                                            if inner_invocation.name == macro_name {
+                                                inner_invocation.name = new_macro_name.to_string();
+                                                is_defined = true;
+                                            }
                                         }
+                                        SyntaxToken::QuotedString(quoted_string) => {
+                                            if quoted_string == macro_name {
+                                                *quoted_string = new_macro_name.to_string();
+                                                is_defined = true;
+                                            }
+                                        }
+                                        _ => {}
                                     }
                                 }
                             }
                         }
+                    } else if is_undefinition(invocation, macro_name) {
+                        is_defined = false;
                     }
                 }
             }
